9d3dad70bdd8ee4338aaae98d7e676a52fe03a3e
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-08-02 16:33:50 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,14 @@
+# class josh():
+#     course1="core Python"
+#     course2="Adv Python Programming"
+# print(josh.course1)                  # core Python
+# print(josh.course2)                  # Adv Python Programming
+# class bangaram():
+#     course1="python"
+#     course2="full stock"
+# print(bangaram.course1)               #  python
+# print(bangaram.course2)                # full stock
+# p=bangaram()
+# print(p.course1)                       #  python
+# print(p.course2)                       #  full stock
+# print(p.course1+" ,"+p.course2)        #  python ,full stock
